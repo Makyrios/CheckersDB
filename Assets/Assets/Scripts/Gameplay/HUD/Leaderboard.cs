bcb7ad1317ef3a8a8ffd7b16f9dbde3f389f84cf
Makyrios <lavreniuk.mykyta@lll.kpi.ua>
2022-12-31 16:08:36 +0200

    Added sort by rating to leaderboard

--- Assets/Assets/Scripts/Gameplay/HUD/Leaderboard.cs
+++ Assets/Assets/Scripts/Gameplay/HUD/Leaderboard.cs
@@ -1,3 +1,6 @@
+using GameClasses;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TMPro;
 using UnityEngine;
@@ -7,9 +10,12 @@ public class Leaderboard : MonoBehaviour
 {
     public TextMeshProUGUI usernameColumn;
     public TextMeshProUGUI scoreColumn;
+    private List<BaseGameAccount> sortedUsers;
 
     private void Start()
     {
+        sortedUsers = DataBaseInitializer.singleton.userService.SelectAllUsers().ToList<BaseGameAccount>();
+        sortedUsers.Sort((a, b) => b.CurrentRating.CompareTo(a.CurrentRating));
         ShowUsernames();
         ShowScores();
     }
@@ -17,7 +23,8 @@ public class Leaderboard : MonoBehaviour
     private void ShowUsernames()
     {
         StringBuilder usernames = new StringBuilder();
-        foreach (var user in DataBaseInitializer.singleton.userService.SelectAllUsers())
+
+        foreach (var user in sortedUsers)
         {
             usernames.AppendLine(user.Username);
         }
@@ -27,7 +34,7 @@ public class Leaderboard : MonoBehaviour
     private void ShowScores()
     {
         StringBuilder usernames = new StringBuilder();
-        foreach (var user in DataBaseInitializer.singleton.userService.SelectAllUsers())
+        foreach (var user in sortedUsers)
         {
             usernames.AppendLine(user.CurrentRating.ToString());
         }
